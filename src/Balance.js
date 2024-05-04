fb2caa3c4d1fd5710d674877616f7612577da26c
yeti103 <sanskarsharma987@gmail.com>
2024-05-04 19:44:55 +0530

    initial compmonent

---
+++ src/Balance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Balance() {
+  return (
+    <div>
+        <div>
+        </div>
+        <div>
+        </div>
+    </div>
+  )
+}
+
+export default Balance
---
+++ src/IncomeExpenses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function IncomeExpenses() {
+  return (
+    <div>IncomeExpenses</div>
+  )
+}
+
+export default IncomeExpenses
