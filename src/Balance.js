fb5d95ce9a063548851c95a7336d96ff3acfbbc1
yeti103 <sanskarsharma987@gmail.com>
2024-06-14 23:42:28 +0530

    added color change of spending bar and button

--- src/Balance.js
+++ src/Balance.js
@@ -5,14 +5,14 @@ import { GlobalContext } from './context/GlobalState';
 function Balance() {
   const {transactions}=useContext(GlobalContext);
   const limit=5000;
-  const[spend_percent,setSpend_Percent]=useState("1%")
+  const[spend_percent,setSpend_Percent]=useState(0)
    const total_balance =
      transactions.length > 0
        ? transactions.reduce((acc, transaction) => acc + transaction.amount, 0)
        : 0;
   
   useEffect(()=>{
-    setSpend_Percent((total_balance/limit)*100+"%");
+    setSpend_Percent((total_balance/limit)*100);
   },[total_balance])
   return (
     <div className='Balance'>
@@ -20,7 +20,7 @@ function Balance() {
            {total_balance}
         </div>
         <div className='total_avail'>
-          <div className='spended' style={{width:spend_percent,height:"100%"}}>
+          <div className={spend_percent<80?'spended-less':'spended-more'} style={{width:spend_percent+'%',height:"100%"}}>
 
           </div>
         </div>
